286c9c9aae096ebcdbfc86275ac59e7925eff2b1
gana0201 <155277541gaeun@gmail.com>
2026-04-23 21:37:07 +0900

    에러 해결

--- clotho-tarot/next.config.ts
+++ clotho-tarot/next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // ✨ CORS 에러 우회를 위한 Proxy 설정
-  async rewrites() {
-    return [
-      {
-        // 프론트엔드에서 이 주소로 요청을 보내면
-        source: '/api/server/:path*', 
-        // Next.js 서버가 몰래 Vercel 서버로 토스해 줍니다
-        destination: 'https://clotho-server-vyw7.vercel.app/api/:path*', 
-      },
-    ];
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          // 프론트엔드에서 이 주소로 요청을 보내면
+          source: '/api/server/:path*', 
+          // Next.js 서버가 몰래 Vercel 서버로 토스해 줍니다
+          destination: 'https://clotho-server-vyw7.vercel.app/api/:path*', 
+        },
+      ]
+    };
   },
 };
 
